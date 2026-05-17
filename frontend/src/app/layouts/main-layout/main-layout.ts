d8f5365a89667b09c6be4aa17bd5a9f3c1d19077
Paul <if24b042@technikum-wien.at>
2026-05-17 16:02:56 +0200

    Added Sidebar and updated header

--- frontend/src/app/layouts/main-layout/main-layout.ts
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../footer/footer';
-import { HeaderComponent } from '../../header/header'
+import { HeaderComponent } from '../../header/header';
+import { SidebarComponent } from '../../sidebar/sidebar';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, SidebarComponent],
   templateUrl: './main-layout.html',
 })
 export class MainLayoutComponent {}
---
+++ frontend/src/app/sidebar/sidebar.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.html',
+})
+export class SidebarComponent {
+  navItems = [
+    { path: '/', icon: 'dashboard', label: 'Dashboard' },
+    { path: '/tour-planner', icon: 'explore', label: 'Tour Planner' },
+    { path: '/logs', icon: 'history_edu', label: 'My Logs' },
+    { path: '/statistics', icon: 'analytics', label: 'Statistics' },
+    { path: '/settings', icon: 'settings', label: 'Settings' }
+  ];
+}
