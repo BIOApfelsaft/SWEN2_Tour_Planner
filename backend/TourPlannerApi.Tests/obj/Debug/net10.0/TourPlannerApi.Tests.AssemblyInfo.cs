2eb7025f39f3fcd6eb26541ddca6fcc5a8edbd87
ItisMaci <macsch.19@gmail.com>
2026-07-05 14:15:49 +0200

    Fixed first line of tour-log-modal.component.html as the component scaled outside of it's intended area.

--- backend/TourPlannerApi.Tests/obj/Debug/net10.0/TourPlannerApi.Tests.AssemblyInfo.cs
+++ backend/TourPlannerApi.Tests/obj/Debug/net10.0/TourPlannerApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourPlannerApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41ea01915e0f401c375bae7c6f834b81ebb5a30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f5865fb2d218ba19126c792a60759e1addf1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourPlannerApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourPlannerApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
